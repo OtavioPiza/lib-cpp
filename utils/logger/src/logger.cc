42531c5e87c39c0d5a51c3252e5be54cb132b3be
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2022-12-28 15:03:33 -0300

    updated logger to use cstrings

--- utils/logger/src/logger.cc
+++ utils/logger/src/logger.cc
@@ -4,41 +4,41 @@
 #include "streams.h"
 
 // See logger.h for documentation.
-void ostp::libcc::utils::log_info(const std::string &msg, const std::string &sender)
+void ostp::libcc::utils::log_info(const char msg[], const char sender[])
 {
     fprintf(INFO_STREAM,
             NORMAL
             "[%s] INFO: %s"
             NORMAL,
-            sender.c_str(), msg.c_str());
+            sender, msg);
 }
 
 // See logger.h for documentation.
-void ostp::libcc::utils::log_success(const std::string &msg, const std::string &sender)
+void ostp::libcc::utils::log_success(const char msg[], const char sender[])
 {
     fprintf(INFO_STREAM,
             GREEN
             "[%s] SUCCESS: %s"
             NORMAL,
-            sender.c_str(), msg.c_str());
+            sender, msg);
 }
 
 // See logger.h for documentation.
-void ostp::libcc::utils::log_warn(const std::string &msg, const std::string &sender)
+void ostp::libcc::utils::log_warn(const char msg[], const char sender[])
 {
     fprintf(INFO_STREAM,
             YELLOW
             "[%s] WARN: %s"
             NORMAL,
-            sender.c_str(), msg.c_str());
+            sender, msg);
 }
 
 // See logger.h for documentation.
-void ostp::libcc::utils::log_error(const std::string &msg, const std::string &sender)
+void ostp::libcc::utils::log_error(const char msg[], const char sender[])
 {
     fprintf(ERROR_STREAM,
             RED
             "[%s] ERROR: %s"
             NORMAL,
-            sender.c_str(), msg.c_str());
+            sender, msg);
 }
