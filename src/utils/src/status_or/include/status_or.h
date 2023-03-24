e057d8aa706c6e1710358927cd5803424902404a
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-03-24 13:17:22 -0400

    done

--- src/utils/src/status_or/include/status_or.h
+++ src/utils/src/status_or/include/status_or.h
@@ -29,10 +29,10 @@ namespace ostp::libcc::utils
         // Methods to check the status of the operation.
 
         /// Returns true if the operation was successful.
-        bool ok() const { return status == Status::OK; }
+        bool ok() const { return status == Status::SUCCESS; }
 
         /// Returns true if the operation failed.
-        bool failed() const { return status != Status::OK; }
+        bool failed() const { return status != Status::SUCCESS; }
     };
 }
 
